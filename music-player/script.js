02163f3ffcb3e080229d4ac780e5c5e1e742e524
carelmaeda <carelmaeda@gmail.com>
2023-09-03 13:46:23 -0400

    feat: stopwatch

--- music-player/script.js
+++ music-player/script.js
@@ -1,33 +1,41 @@
-let toastBox = document.getElementById('toastBox');
-let successMsg = 'This is a success message!'
-let errorMsg = 'This is an error message!'
-let infoMsg = 'This is an informative message!'
+let progress = document.getElementById('progress');
+let song = document.getElementById('song');
+let ctrlIcon = document.getElementById('ctrlIcon');
 
 
-
-function showToast(msg){
-   let toast = document.createElement('div');
-   toast.classList.add('alert');
-   toast.classList.add('alert-success');
-   toast.innerHTML = msg;
-   toastBox.appendChild(toast);
-
-//if the msg has different text it displays notification accordingly
-
-   if(msg.includes('error')){
-    toast.classList.add('alert-danger');
-}
-    if(msg.includes('informative')){
-    toast.classList.add('alert-warning');
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
 }
 
-setTimeout(() => {
-    toast.remove();
-    
-}, 6000);
+//PLAY and PAUSE
 
+function playPause(){
+    if(ctrlIcon.classList.contains("fa-pause")){
+        song.pause();
+        ctrlIcon.classList.remove("fa-pause");
+        ctrlIcon.classList.add("fa-play");
+    }else {
+        song.play();
+        ctrlIcon.classList.add("fa-pause");
+        ctrlIcon.classList.remove("fa-play");
+    }
 }
 
+//Progress Bar
 
 
+if(song.play()){
+    setInterval(()=>{
+        progress.value = song.currentTime;
+    },500);
+}
 
+//click anywhere to progress
+
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value;
+    ctrlIcon.classList.add("fa-pause");
+    ctrlIcon.classList.remove("fa-play");
+}
